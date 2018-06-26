ad78b7bdb00a13710d27e3bf3f3378d9f2e1d3cc
korlando7 <korlando7@gmail.com>
2018-06-26 11:34:08 -0400

    checking to see if message is string

--- util/renderer.js
+++ util/renderer.js
@@ -2,7 +2,7 @@ const emoji = require('node-emoji')
 
 // adds any additional processing for the message text
 const render = (m) => {
-  if (m) {
+  if (typeof m === 'string') {
     // rendering emojis into messages
     const emojified = emoji.emojify(m, null, e => `${e} `)
 
