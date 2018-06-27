d330a44a6ea9195af593b9747bacc7f976902d96
korlando7 <korlando7@gmail.com>
2018-06-26 23:26:41 -0400

    adding emoji tab completion

--- util/tabComplete.js
+++ util/tabComplete.js
@@ -1,16 +1,21 @@
 const state = require('./state')
 const commander = require('./commander')
+const emojis = require('node-emoji')
 
 const author = (partial) => {
   const authors = state.getAuthors()
   const names = Object.values(authors).map(obj => obj.name)
-
   return names.filter(name => name.startsWith(partial))
 }
 
 const command = (partial) => {
   const commands = Object.keys(commander.commands)
   return commands.filter(cmd => cmd.startsWith(partial))
+}
+
+const emoji = (partial) => {
+  const emojiList = Object.keys(emojis.emoji)
+  return emojiList.filter(em => em.startsWith(partial))
 }
 
 module.exports = (line) => {
@@ -21,6 +26,10 @@ module.exports = (line) => {
 
   if (split.length === 1 && lastWord.indexOf('/') === 0) { // command
     matches = command(lastWord)
+  } else if (split.length === 1 && lastWord.indexOf(':') === 0) {
+    matches = emoji(lastWord.slice(1))
+    let idx = -1
+    return () => (matches.length && `:${matches[++idx % matches.length]}:`) || line
   } else {
     // if they didn't include an @ symbol, put one on
     if (lastWord.indexOf('@') !== 0) {
