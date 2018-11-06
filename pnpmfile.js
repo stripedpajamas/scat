5dd531e1ba961865923fcec4a102984deb51cce6
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-11-06 15:58:32 -0500

    Update pnpmfile

--- pnpmfile.js
+++ pnpmfile.js
@@ -12,9 +12,7 @@ module.exports = {
           context.log(`Added ssb-keys@${ssbKeysVersion} to ssb-validate dependencies`)
           break
         }
-        case 'ssb-ws':
-        case 'ssb-names':
-        case 'ssb-meme': {
+        case 'ssb-names': {
           const pullStreamVersion = pkg.devDependencies['pull-stream'] || '^3.6.8'
           pkg.dependencies = {
             ...pkg.dependencies,
@@ -22,6 +20,23 @@ module.exports = {
           }
           delete pkg.devDependencies['pull-stream']
           context.log(`Added pull-stream@${pullStreamVersion} to ${pkg.name} dependencies`)
+          break
+        }
+        case 'ssb-friends': {
+          pkg.dependencies = {
+            ...pkg.dependencies,
+            'pull-notify': '^0.1.1'
+          }
+          context.log('Added pull-notify@$^0.1.1 to ssb-friends dependencies')
+          break
+        }
+        case 'layered-graph': {
+          pkg.dependencies = {
+            ...pkg.dependencies,
+            'pull-stream': '^3.6.9'
+          }
+          context.log('Added pull-stream@^3.6.9 to layered-graph dependencies')
+          break
         }
         default:
           break
