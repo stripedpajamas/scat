e2e7daee7c0669cc90d1df68dc539ddeb44bb846
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-07-31 12:03:15 -0700

    Functional

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const Diffy = require('diffy')
+const input = require('diffy/input')()
+const pino = require('pino')
 const pull = require('pull-stream')
 const connect = require('ssb-client')
 const colors = require('colorette')
 
-const { gray, whiteBright: white, bold } = colors
+const dbg = pino(pino.destination(2)) // log to stderr
+
+const constants = {
+  SYSTEM: '<scat>',
+  DATE_TIME_OPTS: {
+    month: 'numeric',
+    year: 'numeric',
+    day: 'numeric',
+    hour: 'numeric', 
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+  },
+  MESSAGE_TYPE: 'scat_message',
+  MODE: {
+    PUBLIC: 'PUBLIC',
+    PRIVATE: 'PRIVATE'
+  },
+  PROGRAM_DIR: '.scat',
+  TIME_WINDOW: 7 * 24 * 60 * 60 * 1000, // 7 days
+}
+
+const { gray, whiteBright: white, bold, black, bgWhite, bgYellow } = colors
 
 const colorMap = {}
+const nameMap = {}
 
 function randomColor () {
   const colorList = ['green', 'cyan', 'magenta', 'blue', 'red']
@@ -20,28 +45,7 @@ function getAuthorColor (author) {
 }
 
 function colorAuthor (author) {
-  return `${bold(colors[getAuthorColor()](author))}`
-}
-
-constants = {
-  DATE_TIME_OPTS: {
-    month: 'numeric',
-    year: 'numeric',
-    day: 'numeric',
-    hour: 'numeric', 
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false
-  },
-  MESSAGE_TYPE: 'scat_message',
-  ABOUT: 'about',
-  CONTACT: 'contact',
-  MODE: {
-    PUBLIC: 'PUBLIC',
-    PRIVATE: 'PRIVATE'
-  },
-  PROGRAM_DIR: '.scat',
-  TIME_WINDOW: 7 * 24 * 60 * 60 * 1000, // 7 days
+  return `${bold(colors[getAuthorColor(author)](author))}`
 }
 
 function getMaxVisibleMsgs () {
@@ -53,58 +57,109 @@ function getSpacer (msgsLength) {
 }
 
 function getPrintableMsgs (msgs) {
-  return msgs.map(({ author, timestamp, text }) => `[${gray(timestamp)}] ${colorAuthor(author)}: ${white(text)}`).join('\n')
+  return msgs.map(({ author, timestamp, text }) => {
+    const textFormatted = author === constants.SYSTEM ? bgYellow(black(text)) : white(text)
+    return `[${gray(timestamp)}] ${colorAuthor(author)}: ${textFormatted}`
+  }).join('\n')
 }
 
-function processor (diffy) {
+function ts (timestamp) {
+  const ts = new Date(timestamp)
+  return new Intl.DateTimeFormat('default', constants.DATE_TIME_OPTS).format(ts) // TODO allow timezone to be configured
+}
+
+async function processor (diffy, server) {
   const msgs = []
+  const meId = server.id
 
   // setup ui
-  diffy.render(() => `${getPrintableMsgs(msgs)}${getSpacer(msgs.length)}\nINPUT MIGHT GO HERE >>>`)
+  diffy.render(() => `${getPrintableMsgs(msgs)}${getSpacer(msgs.length)}\n> ${input.line()}`)
 
-  return (msg) => {
-    if (msgs.length >= getMaxVisibleMsgs()) {
-      msgs.shift()
+  return {
+    incoming: (msg) => {
+      if (msgs.length >= getMaxVisibleMsgs()) {
+        msgs.shift()
+      }
+      if (!msg || !msg.value || !msg.value.content || msg.value.content.type !== constants.MESSAGE_TYPE) {
+        return
+      }
+      if (!nameMap[msg.value.author] && server.about) {
+        server.about.socialValue({ key: 'name', dest: msg.value.author })
+          .then((authorName) => {
+            nameMap[msg.value.author] = authorName
+            for (let existingMsg of msgs) {
+              if (existingMsg.author === msg.value.author) existingMsg.author = authorName
+            }
+            if (colorMap[msg.value.author]) colorMap[authorName] = colorMap[msg.value.author]
+            diffy.render()
+          })
+          .catch((err) => {
+            dbg.error(err)
+          })
+      }
+        
+      const scatMsg = {
+        timestamp: ts(msg.value.timestamp),
+        author: nameMap[msg.value.author] || msg.value.author,
+        text: msg.value.content.text
+      }
+      msgs.push(scatMsg)
+      diffy.render()
+    },
+    outgoing: (line) => {
+      // handle commands
+      if (line && line[0] === '/') {
+        const wordBreak = line.indexOf(' ')
+        const cmdEnd = wordBreak > 0 ? wordBreak : line.length
+        switch (line.substring(1, cmdEnd)) {
+          case 'debug': {
+            dbg.info({ colorMap, msgs })
+            break
+          }
+        }
+      } else {
+        server.publish({ type: constants.MESSAGE_TYPE, text: line })
+          .catch((err) => {
+            msgs.push({ timestamp: ts(Date.now()), author: constants.SYSTEM, text: 'Failed to post message.' })
+            diffy.render()
+            dbg.error(err)
+          })
+      }
     }
-    if (!msg || !msg.value || !msg.value.content || msg.value.content.type !== constants.MESSAGE_TYPE) {
-      return
-    }
-    const ts = new Date(msg.value.timestamp)
-    const scatMsg = {
-      timestamp: new Intl.DateTimeFormat('default', constants.DATE_TIME_OPTS).format(ts), // TODO allow timezone to be configured
-      author: msg.value.author,
-      text: msg.value.content.text
-    }
-    msgs.push(scatMsg)
-    diffy.render()
   }
 }
 
 async function main () {
-  connect((err, server) => {
-    if (err) {
-      console.error(err)
-      return
-    }
+  let server
+  try {
+    server = await connect()
+  } catch (err) {
+    console.error(err)
+  }
 
-    const since = Date.now() - constants.TIME_WINDOW
-    const diffy = Diffy({ fullscreen: true })
-    pull(
-      server.query.read({
-        reverse: true,
-        live: true,
-        query: [{
-          $filter: {
-            value: {
-              content: { type: constants.MESSAGE_TYPE },
-              timestamp: { $gte: since }
-            }
+  const since = Date.now() - constants.TIME_WINDOW
+  const diffy = Diffy({ fullscreen: true })
+  const { incoming, outgoing } = await processor(diffy, server)
+  // setup input listeners
+  input.on('update', () => diffy.render())
+  input.on('enter', outgoing)
+
+  // pull data from sbot
+  pull(
+    server.query.read({
+      reverse: true,
+      live: true,
+      query: [{
+        $filter: {
+          value: {
+            content: { type: constants.MESSAGE_TYPE },
+            timestamp: { $gte: since }
           }
-        }]
-      }),
-      pull.drain(processor(diffy))
-    )
-  })
+        }
+      }]
+    }),
+    pull.drain(incoming)
+  )
 }
 
 main()
