84d0da61f8fcd6da606cfb225e00194d693973a4
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-07-30 17:48:25 -0700

    Initial

--- index.js
+++ index.js
@@ -1,48 +1,110 @@
-#!/usr/bin/env node
-const updateNotifier = require('update-notifier')
-const clp = require('clp')
-const core = require('ssb-chat-core')
-const state = require('./util/state')
-const ui = require('./util/ui')
-const pkg = require('./package.json')
+const Diffy = require('diffy')
+const pull = require('pull-stream')
+const connect = require('ssb-client')
+const colors = require('colorette')
 
-const args = clp()
-// set emoji arg
-args.emoji = !(args.emoji === false)
-// put args into state
-state.setArgs(args)
+const { gray, whiteBright: white, bold } = colors
 
-updateNotifier({
-  pkg,
-  updateCheckInterval: 1000 * 60 * 60 * 12 // 12 hours
-}).notify({
-  defer: true,
-  isGlobal: true
-})
+const colorMap = {}
 
-process.on('uncaughtException', (e) => {
-  if (args.debug) {
-    console.log(e)
-  } else {
-    console.log('\n\nUncaught exception, exiting :(')
+function randomColor () {
+  const colorList = ['green', 'cyan', 'magenta', 'blue', 'red']
+  return colorList[Math.floor(Math.random() * colorList.length)]
+}
+
+function getAuthorColor (author) {
+  if (!colorMap[author]) {
+    colorMap[author] = randomColor()
   }
-  core.stop()
-  process.exit(1)
-})
+  return colorMap[author]
+}
 
-// set up listener for mode changes so we can clear system/error msgs
-core.events.on('mode-changed', (mode) => {
-  state.resetSystemMessage()
-})
+function colorAuthor (author) {
+  return `${bold(colors[getAuthorColor()](author))}`
+}
 
-core.start({
-  debug: !!args.debug,
-  timeWindow: args.days ? args.days * 24 * 60 * 60 * 1000 : undefined
-}, (err) => {
-  if (err) {
-    console.log(err)
-    process.exit(1)
+constants = {
+  DATE_TIME_OPTS: {
+    month: 'numeric',
+    year: 'numeric',
+    day: 'numeric',
+    hour: 'numeric', 
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+  },
+  MESSAGE_TYPE: 'scat_message',
+  ABOUT: 'about',
+  CONTACT: 'contact',
+  MODE: {
+    PUBLIC: 'PUBLIC',
+    PRIVATE: 'PRIVATE'
+  },
+  PROGRAM_DIR: '.scat',
+  TIME_WINDOW: 7 * 24 * 60 * 60 * 1000, // 7 days
+}
+
+function getMaxVisibleMsgs () {
+  return process.stdout.rows - 3
+}
+
+function getSpacer (msgsLength) {
+  return '\n'.repeat(getMaxVisibleMsgs() - msgsLength)
+}
+
+function getPrintableMsgs (msgs) {
+  return msgs.map(({ author, timestamp, text }) => `[${gray(timestamp)}] ${colorAuthor(author)}: ${white(text)}`).join('\n')
+}
+
+function processor (diffy) {
+  const msgs = []
+
+  // setup ui
+  diffy.render(() => `${getPrintableMsgs(msgs)}${getSpacer(msgs.length)}\nINPUT MIGHT GO HERE >>>`)
+
+  return (msg) => {
+    if (msgs.length >= getMaxVisibleMsgs()) {
+      msgs.shift()
+    }
+    if (!msg || !msg.value || !msg.value.content || msg.value.content.type !== constants.MESSAGE_TYPE) {
+      return
+    }
+    const ts = new Date(msg.value.timestamp)
+    const scatMsg = {
+      timestamp: new Intl.DateTimeFormat('default', constants.DATE_TIME_OPTS).format(ts), // TODO allow timezone to be configured
+      author: msg.value.author,
+      text: msg.value.content.text
+    }
+    msgs.push(scatMsg)
+    diffy.render()
   }
+}
 
-  ui({ debug: !!args.debug, quiet: !!args.quiet })
-})
+async function main () {
+  connect((err, server) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    const since = Date.now() - constants.TIME_WINDOW
+    const diffy = Diffy({ fullscreen: true })
+    pull(
+      server.query.read({
+        reverse: true,
+        live: true,
+        query: [{
+          $filter: {
+            value: {
+              content: { type: constants.MESSAGE_TYPE },
+              timestamp: { $gte: since }
+            }
+          }
+        }]
+      }),
+      pull.drain(processor(diffy))
+    )
+  })
+}
+
+main()
